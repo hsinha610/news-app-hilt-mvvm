62fdf52ed59a39735d680b173da1f9d5a4e6b613
hsinha610 <hsinha610@gmail.com>
2023-05-18 01:26:55 +0530

    Changed status bar color

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.newsapp
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newsapp.databinding.ActivityMainBinding
 import com.example.newsapp.ui.NewsListFragment
@@ -15,7 +17,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initStatusBarColor()
         initFragment()
+    }
+
+    private fun initStatusBarColor() {
+        val window = this.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = Color.parseColor("#202021")
     }
 
     private fun initFragment() {
